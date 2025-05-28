982dc669c248eeb3e639014c25d429bab8314081
reginetan <reginetan297@gmail.com>
2025-05-28 16:06:32 +0800

    Finished get products

--- app/utils/header.php
+++ app/utils/header.php
@@ -1,6 +1,6 @@
 <head>
   <title>Crumbly</title>
-  <link rel="icon" type="image/png" href="images/icon-small.png">
+  <link rel="icon" type="image/png" href="images/logo-small.png">
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
