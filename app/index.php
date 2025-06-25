4f7397a17150cc8620656a8b2439a2ffce3aa4bd
reginetan <reginetan297@gmail.com>
2025-06-25 17:21:57 +0800

    Added category filters

--- app/index.php
+++ app/index.php
@@ -25,7 +25,7 @@
           <h2 class="section-title">Category</h2>
 
           <div class="d-flex align-items-center">
-            <a href="#" class="btn-link text-decoration-none">View All Categories →</a>
+            <a href="category.php?category=Cakes" class="btn-link text-decoration-none">View All Categories →</a>
             <div class="swiper-buttons">
               <button class="swiper-prev category-carousel-prev btn btn-yellow">❮</button>
               <button class="swiper-next category-carousel-next btn btn-yellow">❯</button>
@@ -37,42 +37,40 @@
     </div>
     <div class="row">
       <div class="col-md-12">
-
         <div class="category-carousel swiper">
           <div class="swiper-wrapper">
-            <a href="index.html" class="nav-link category-item swiper-slide text-center">
+            <a href="category.php?category=Cakes" class="nav-link category-item swiper-slide text-center">
               <img src="images/icon-cake.png" alt="Category Thumbnail"
                 style="width: 50px; height: 50px; display: block; margin: 0 auto;">
               <h3 class="category-title">Cakes</h3>
             </a>
-            <a href="index.html" class="nav-link category-item swiper-slide text-center">
+            <a href="category.php?category=Tarts" class="nav-link category-item swiper-slide text-center">
               <img src="images/icon-tart.png" alt="Category Thumbnail"
                 style="width: 50px; height: 50px; display: block; margin: 0 auto;">
               <h3 class="category-title">Tarts</h3>
             </a>
-            <a href="index.html" class="nav-link category-item swiper-slide text-center">
+            <a href="category.php?category=Cookies" class="nav-link category-item swiper-slide text-center">
               <img src="images/icon-cookie.png" alt="Category Thumbnail"
                 style="width: 50px; height: 50px; display: block; margin: 0 auto;">
               <h3 class="category-title">Cookies</h3>
             </a>
-            <a href="index.html" class="nav-link category-item swiper-slide text-center">
+            <a href="category.php?category=Cupcakes" class="nav-link category-item swiper-slide text-center">
               <img src="images/icon-cupcake.png" alt="Category Thumbnail"
                 style="width: 50px; height: 50px; display: block; margin: 0 auto;">
               <h3 class="category-title">Cupcakes</h3>
             </a>
-            <a href="index.html" class="nav-link category-item swiper-slide text-center">
+            <a href="category.php?category=Muffins" class="nav-link category-item swiper-slide text-center">
               <img src="images/icon-muffin.png" alt="Category Thumbnail"
                 style="width: 50px; height: 50px; display: block; margin: 0 auto;">
               <h3 class="category-title">Muffins</h3>
             </a>
-            <a href="index.html" class="nav-link category-item swiper-slide text-center">
+            <a href="category.php?category=Puffs" class="nav-link category-item swiper-slide text-center">
               <img src="images/icon-puff.png" alt="Category Thumbnail"
                 style="width: 50px; height: 50px; display: block; margin: 0 auto;">
               <h3 class="category-title">Puffs</h3>
             </a>
           </div>
         </div>
-
       </div>
     </div>
   </div>
@@ -956,6 +954,7 @@
 
 
 
-      <?php include 'utils/footer.php'; ?>
+<?php include 'utils/footer.php'; ?>
 </body>
+
 </html>
