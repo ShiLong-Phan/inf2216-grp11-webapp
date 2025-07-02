40f32804c772b7ea2b84545c260d5af1ddc99314
Auston <realauston@gmail.com>
2025-07-02 19:58:13 +0800

    removed lorem ipsum on the "get 25% discount on your first purchase" section and tidied up the footer to remove all the random links and what not and simplified it to just show the logo and the all rights reserved

--- app/utils/footer.php
+++ app/utils/footer.php
@@ -1,16 +1,37 @@
 <!-- Footer divider -->
 <div class="footer-divider">
   <div class="container-fluid">
-    <hr class="footer-separator" style="height: 3px; background-color: #dee2e6; border: none; margin-bottom: 0;">
+    <hr class="footer-separator"
+        style="height: 3px; background-color: #dee2e6; border: none; margin-bottom: 0;">
   </div>
 </div>
 
+<footer id="footer-bottom" class="py-3">
+  <div class="container-fluid">
+    <div class="row justify-content-center align-items-center">
+      
+      <!-- Logo -->
+      <div class="col-auto">
+        <img src="images/logo.png" alt="Crumbly Logo" class="footer-logo" style="height: 80px; width: auto; margin-right: 0.5rem;">
+      </div>
+
+      <!-- Copyright text -->
+      <div class="col-auto">
+        <p class="mb-0">&copy; 2025 Crumbly. All rights reserved.</p>
+      </div>
+
+    </div>
+  </div>
+</footer>
+
+
+<!--
 <footer class="py-5">
   <div class="container-fluid">
     <div class="row">
       <div class="col-lg-3 col-md-6 col-sm-6">
         <div class="footer-menu">
-          <img src="images/logo.png" style="height: 250px; width: 250px" alt="logo">
+
           <div class="social-links mt-5">
             <ul class="d-flex list-unstyled gap-2">
               <li>
@@ -57,7 +78,6 @@
           </div>
         </div>
       </div>
-
 
       <div class="col-md-2 col-sm-6">
         <div class="footer-menu">
@@ -149,20 +169,4 @@
     </div>
   </div>
 </footer>
-<div id="footer-bottom">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-6 copyright">
-        <p>© 2025 Crumbly. All rights reserved.</p>
-      </div>
-    </div>
-  </div>
-</div>
-<script src="js/jquery-1.11.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-  integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-  crossorigin="anonymous"></script>
-<script src="js/plugins.js"></script>
-<script src="js/script.js"></script>
-</footer>
+-->
