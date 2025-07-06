0c0e6353b934bdae3d6e240cd8c35d8ba062cf67
butlikewhyy <2301871@sit.singaporetech.edu.sg>
2025-07-06 17:32:15 +0800

    offload cookies to WAF

--- app/utils/session.php
+++ app/utils/session.php
@@ -2,14 +2,14 @@
 if (session_status() === PHP_SESSION_NONE) {
 
     // force cookies to be Secure, HttpOnly, sameSite=Lax
-    session_set_cookie_params([
-    'lifetime' => 0, // 0 means until the browser is closed
-    'path'     => '/', // available within the entire domain
-    'domain'   => 'https://crumbly.mooo.com', // crumbly's domain name
-    'secure'   => true,    // HTTPS only
-    'httponly' => true,    // JS can’t read the cookie
-    'samesite' => 'Lax' // CSRF protection
-    ]);
+    // session_set_cookie_params([
+    // 'lifetime' => 0, // 0 means until the browser is closed
+    // 'path'     => '/', // available within the entire domain
+    // 'domain'   => 'crumbly.mooo.com', // crumbly's domain name
+    // 'secure'   => true,    // HTTPS only
+    // 'httponly' => true,    // JS can’t read the cookie
+    // 'samesite' => 'Lax' // CSRF protection
+    // ]);
 
     session_start();
 }
